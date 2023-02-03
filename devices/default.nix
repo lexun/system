88b494c365af59060ddf43b6b05f6cede9c78928
Luke Barbuto <luke.barbuto@gmail.com>
2023-02-03 14:08:50 -0500

    Add work macbook configuration

--- devices/default.nix
+++ devices/default.nix
@@ -1,19 +1,26 @@
 { darwin, flake-utils, home-manager }:
 
+let
+  darwinModules = [
+    ../darwin
+    home-manager.darwinModules.home-manager
+    {
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+      home-manager.users.luke = import ../darwin/home;
+      users.users.luke.home = "/Users/luke";
+    }
+  ];
+in
 {
   darwinConfigurations = {
     "Lukes-Personal-MBP" = darwin.lib.darwinSystem {
       system = flake-utils.lib.system.x86_64-darwin;
-      modules = [
-        ../darwin
-        home-manager.darwinModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.luke = import ../darwin/home;
-          users.users.luke.home = "/Users/luke";
-        }
-      ];
+      modules = darwinModules;
+    };
+    "Lukes-Work-MBP" = darwin.lib.darwinSystem {
+      system = flake-utils.lib.system.aarch64-darwin;
+      modules = darwinModules;
     };
   };
 }
