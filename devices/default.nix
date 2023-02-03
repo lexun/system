23a635cbdd39565df0ef9a62dda64bb7a0d6855f
Luke Barbuto <luke.barbuto@gmail.com>
2023-02-03 13:22:42 -0500

    Rename personal macbook

--- devices/default.nix
+++ devices/default.nix
@@ -1,18 +1,19 @@
 { darwin, flake-utils, home-manager }:
 
 {
-  darwinConfigurations."Lukes-MacBook-Pro" = darwin.lib.darwinSystem {
-    system = flake-utils.lib.system.x86_64-darwin;
-
-    modules = [
-      ../darwin
-      home-manager.darwinModules.home-manager
-      {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.users.luke = import ../darwin/home;
-        users.users.luke.home = "/Users/luke";
-      }
-    ];
+  darwinConfigurations = {
+    "Lukes-Personal-MBP" = darwin.lib.darwinSystem {
+      system = flake-utils.lib.system.x86_64-darwin;
+      modules = [
+        ../darwin
+        home-manager.darwinModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.luke = import ../darwin/home;
+          users.users.luke.home = "/Users/luke";
+        }
+      ];
+    };
   };
 }
