5bd7addee14b6d2124a586eed2d4a020fb35d287
Luke Barbuto <luke.barbuto@gmail.com>
2023-09-30 15:38:23 -0400

    Use latest OTP with Elixir

--- packages/overlay.nix
+++ packages/overlay.nix
@@ -1,5 +1,11 @@
 self: super:
 
+let
+  erlang = super.erlangR26;
+  beamPkgs = super.beam.packagesWith erlang;
+in
+
 {
+  elixir = beamPkgs.elixir_1_15;
   livebook = super.callPackage ./livebook.nix { };
 }
