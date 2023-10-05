73f9f38ef4e53bd3c21009ad70126c646093a6b0
Luke Barbuto <luke.barbuto@gmail.com>
2023-10-05 18:35:49 -0400

    Add welcome text to Phoenix template

--- templates/default.nix
+++ templates/default.nix
@@ -7,6 +7,19 @@
     phoenix = {
       path = ./phoenix;
       description = "Provides a development environment for Phoenix projects";
+      welcomeText = ''
+        # The dev command must be allowed to execute
+
+        ```sh
+          chmod +x scripts/bin/dev
+        ```
+
+        # Merge .gitignore files or run the following command
+
+        ```sh
+          echo -e "\n# Dev environment local state\n/.devenv/\n/.direnv/\n/.local/" >> .gitignore
+        ```
+      '';
     };
   };
 }
