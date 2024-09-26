c47b78864c3aeb95b459a1030d8fed3302dacb4f
Luke Barbuto <luke.barbuto@gmail.com>
2024-09-26 14:53:36 -0700

    Remove outdated templates

--- templates/default.nix
+++ templates/default.nix
@@ -1,25 +1,3 @@
 {
-  flake.templates = {
-    livebook = {
-      path = ./livebook;
-      description = "Provides a development environment for Livebook";
-    };
-    project = {
-      path = ./project;
-      description = "Provides a development environment for generic projects";
-      welcomeText = ''
-        # The dev command must be allowed to execute
-
-        ```sh
-          chmod +x bin/dev
-        ```
-
-        # Merge .gitignore files or run the following command if there is a conflict
-
-        ```sh
-          echo -e "\n# Dev environment local state\n/.devenv/\n/.direnv/\n/.local/" >> .gitignore
-        ```
-      '';
-    };
-  };
+  flake.templates = { };
 }
--- templates/project/shell.nix
+++
@@ -1,5 +0,0 @@
-# Added to support tooling like the Nix Env Selector for VS Code, which allows
-# editor plugins to access pacakges like formatters from the dev env.
-# https://marketplace.visualstudio.com/items?itemName=arrterian.nix-env-selector
-(builtins.getFlake ("git+file://" + toString ./.)
-).devShells.${builtins.currentSystem}.default
