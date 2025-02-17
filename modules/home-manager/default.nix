7cb89868e0c3f7b427457d8e7a391101d8afbdaa
Luke Barbuto <luke.barbuto@gmail.com>
2025-02-16 14:43:45 -1000

    Add Nushell configuration

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -39,8 +39,14 @@
     stateVersion = "23.05";
   };
 
+  programs.carapace = {
+    enable = true;
+    enableNushellIntegration = true;
+  };
+
   programs.direnv = {
     enable = true;
+    enableNushellIntegration = true;
     enableZshIntegration = true;
     nix-direnv.enable = true;
   };
@@ -67,6 +73,52 @@
     enable = true;
     viAlias = true;
     vimAlias = true;
+  };
+
+  programs.nushell = {
+    enable = true;
+    envFile.text = ''
+      $env.config.show_banner = false
+    '';
+    shellAliases = {
+      gaa = "git add --all";
+      gap = "git add --patch";
+      gc = "git commit";
+      gca = "git commit --amend";
+      gcan = "git commit --amend --no-edit";
+      gcana = "git commit --amend --no-edit --all";
+      gcb = "git checkout -b";
+      gcm = "git commit -m";
+      gco = "git checkout";
+      gcp = "git cherry-pick";
+      gd = "git diff";
+      gdca = "git diff --cached";
+      gf = "git fetch";
+      gl = "git pull";
+      glg = "git log";
+      glo = "git log --oneline";
+      gp = "git push";
+      gpf = "git push --force";
+      gpfl = "git push --force-with-lease";
+      gpsup = "git push --set-upstream origin HEAD";
+      grb = "git rebase";
+      grbi = "git rebase -i";
+      grbm = "git rebase main";
+      grh = "git reset HEAD";
+      gs = "git show";
+      gst = "git status";
+      gsta = "git stash";
+      gstp = "git stash pop";
+      la = "ls -a";
+      ll = "ls -l";
+      lx = "ls -la";
+    };
+  };
+
+  programs.oh-my-posh = {
+    enable = true;
+    enableNushellIntegration = true;
+    useTheme = "huvix";
   };
 
   programs.ssh = {
@@ -114,7 +166,6 @@
     oh-my-zsh = {
       enable = true;
       plugins = [ "git" ];
-      theme = "miloshadzic";
     };
   };
 }
