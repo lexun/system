db31c88c4e56a763cfb007cb2a970db172515618
Luke Barbuto <luke.barbuto@gmail.com>
2025-03-09 22:22:46 -1000

    Update dependencies

--- modules/nix-darwin/default.nix
+++ modules/nix-darwin/default.nix
@@ -50,7 +50,7 @@
 
   programs.zsh.enable = true;
 
-  security.pam.enableSudoTouchIdAuth = true;
+  security.pam.services.sudo_local.touchIdAuth = true;
 
   system.keyboard.enableKeyMapping = true;
   system.keyboard.remapCapsLockToEscape = true;
