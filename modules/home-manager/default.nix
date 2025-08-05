0ad3e7de3e8c96dc93a6c24d8060aae6d090e5f4
Luke Barbuto <luke.barbuto@gmail.com>
2025-08-05 13:26:17 -1000

    Launch into zsm on coder instances

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -290,7 +290,7 @@
           StrictHostKeyChecking = "no";
           UserKnownHostsFile = "/dev/null";
           LogLevel = "ERROR";
-          RemoteCommand = "bash -c 'source ~/.nix-profile/etc/profile.d/hm-session-vars.sh 2>/dev/null || true; exec zellij attach --create remote'";
+          RemoteCommand = "bash -c 'source ~/.nix-profile/etc/profile.d/hm-session-vars.sh 2>/dev/null || true; exec zsm'";
           RequestTTY = "yes";
         };
         proxyCommand = "sh -c '${pkgs.coder}/bin/coder --global-config \"/Users/luke/Library/Application Support/coderv2\" ssh --stdio --hostname-suffix coder %h 2>/dev/null'";
