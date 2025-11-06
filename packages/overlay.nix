b7ebe9200e16e2ee13747322867c3a855bcb318b
Luke Barbuto <luke.barbuto@gmail.com>
2025-11-06 07:53:38 -1000

    Remove carapace override

--- packages/overlay.nix
+++ packages/overlay.nix
@@ -3,15 +3,4 @@ final: prev: {
   git-coauthor = prev.callPackage ./git-coauthor { };
   system-update = prev.callPackage ./system-update { };
   zsm = prev.callPackage ./zsm { };
-
-  carapace = prev.carapace.overrideAttrs (oldAttrs: {
-    version = "1.4.1";
-    src = prev.fetchFromGitHub {
-      owner = "carapace-sh";
-      repo = "carapace-bin";
-      tag = "v1.4.1";
-      hash = "sha256-aI69LQuyXUGqxjcUIH3J8AG7cgn/onBg0mZc+zz+YrA=";
-    };
-    vendorHash = "sha256-AwR+Oh1Rlg1z/pYdc9VDvp/FLH1ZiPsP/q4lks3VqqE=";
-  });
 }
